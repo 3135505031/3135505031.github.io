aee97a6a1361ac7573120315d430aabf781dfde6
3135505031 <Syh3135505031@outlook.com>
2023-12-19 13:39:29 +0800

    Site updated: 2023-12-19 13:39:29

--- dist/music.js
+++ dist/music.js
@@ -2,7 +2,7 @@ const ap = new APlayer({
     container: document.getElementById('aplayer'),
     fixed: true,
     autoplay: true,
-    loop: all,
+    loop: 'all',
     volume: 0.7,
     mutex: true,
     lrcType: 3,
