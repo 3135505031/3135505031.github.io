2619fe84e28fef2bcfb008012e08645eef8a6b02
3135505031 <Syh3135505031@outlook.com>
2023-12-19 13:32:57 +0800

    Site updated: 2023-12-19 13:32:57

--- dist/music.js
+++ dist/music.js
@@ -8,18 +8,11 @@ const ap = new APlayer({
     lrcType: 3,
     audio: [
         {
-            name: '花の塔',
-            artist: 'さユり',
+            name: 'You',
+            artist: '雪野五月',
             url: 'http://music.163.com/song/media/outer/url?id=672188.mp3',
             cover: 'http://p1.music.126.net/KQvKhAx7nOKlgGP5MvOiDw==/561850441805463.jpg?param=130y130',
             lrc: 'https://lav2.top/music/lrc/You.lrc'
-        },
-        {
-            name: '花の塔',
-            artist: 'さユり',
-            url: 'http://music.163.com/song/media/outer/url?id=1956534872.mp3',
-            cover: 'https://p2.music.126.net/fS_5RbP_4qg-RYreqp2tGg==/109951167558017839.jpg?param=130y130',
-            lrc: 'https://moechun.fun/music/lrc/さユり - 花の塔.lrc'
         },
     ]
 });
