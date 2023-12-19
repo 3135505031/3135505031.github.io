355249754299984bc0987487b88d3fad94857e37
3135505031 <Syh3135505031@outlook.com>
2023-12-19 17:31:12 +0800

    Site updated: 2023-12-19 17:31:12

--- dist/music.js
+++ dist/music.js
@@ -14,5 +14,12 @@ const ap = new APlayer({
             cover: 'http://p1.music.126.net/KQvKhAx7nOKlgGP5MvOiDw==/561850441805463.jpg?param=130y130',
             lrc: 'https://lav2.top/music/lrc/You.lrc'
         },
+        {
+            name: 'まよいづき',
+            artist: 'onoken / 実咲'.
+            url: 'music.163.com/song/media/outer/url?id=1339750209.mp3',
+            cover: 'http://p2.music.126.net/O8WlM4_YWnHfq7XiR-qpvw==/109951167961999495.jpg?param=130y130',
+            lrc: 'https://lav2.top/music/lrc/まよいづき.lrc',
+        },
     ]
 });
